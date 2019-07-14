88173df5ae2ae512c4d0d0a8b61dd78c80d7c8e7
Lourenz Magoncia <ljmagoncia@yahoo.com>
2019-07-14 07:26:26 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence)
+  reversed_sentence = ""
+  sentence_array = sentence.split(" ")
+  sentence_array.each do |word|
+    word.reverse!
+    
+  end
+  sentence_array
+end
