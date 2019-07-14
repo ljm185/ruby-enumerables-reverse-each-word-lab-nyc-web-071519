fa792d7d7479d0c959963baeb595aa20716a0657
Lourenz Magoncia <ljmagoncia@yahoo.com>
2019-07-14 07:40:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(sentence)
-  reversed_sentence
   sentence_array = sentence.split(" ")
   sentence_array.each do |word|
     word.reverse!
   end
-  sentence
+  reversed_sentence = sentence_array.join(" ")
 end
