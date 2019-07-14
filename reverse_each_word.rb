33e24f1f08cd0afd33c8f8d40c76f84a9a8b0e13
Lourenz Magoncia <ljmagoncia@yahoo.com>
2019-07-14 07:42:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,11 @@ def reverse_each_word(sentence)
   end
   reversed_sentence = sentence_array.join(" ")
 end
+
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(" ")
+  sentence_array.collect do |word|
+    word.reverse!
+  end
+  reversed_sentence = sentence_array.join(" ")
+end
